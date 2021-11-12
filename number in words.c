4828fb1feb8f1d7a69eae7b96f06a780828f62ed
enter-opy <vivekvijayan6282@gmail.com>
2021-11-12 11:29:38 +0530

    Update number in words.c

--- number in words.c
+++ number in words.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <conio.h>
+
+void three_digits(int, char*);
 
 char* digits[] = {
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
@@ -10,7 +13,7 @@ char* teens[] = {
 char* tens[] = {
     "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
 };
-char* power[] = {
+char* powers[] = {
     "",
     "thousand",
     "million",
@@ -55,8 +58,29 @@ int main(void)
     char string[10000];
 
     if (number == 0) printf("%s", digits[number]);
+    else
+    {
+        for (int i = 0; number != 0; i++)
+        {
+            char *three_digit_str[500];
+            three_digits(number % 1000, three_digit_str[i]);
+        }
+    }
 
     getch();
 
     return 0;
+}
+
+void three_digits(int number, char* string)
+{
+    if (number / 100 != 0) strcat(string, strcat(digits[number / 100], " hundred "));
+
+    if (number / 10 % 10 == 1) strcat(string, strcat(teens[number % 10], " "));
+
+    else if (number / 10 % 10 > 1)
+        strcat(string, strcat(tens[number / 10 % 10 - 2], " "));
+        if (number % 10 > 0) strcat(string, strcat(digits[number % 10], " "));
+    
+    if (number % 10 > 0) strcat(string, strcat(digits[number % 10], " "));
 }
